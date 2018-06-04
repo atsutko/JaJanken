e9fa1754b83707d72098e2f60abb8982018bb4d1
atsutko <atsushi.takao.66@gmail.com>
2018-06-04 14:06:25 +0900

    Initial Commit

---
+++ TKJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TKJ
+//
+//  Created by TakaoAtsushi on 2018/06/04.
+//  Copyright © 2018年 TakaoAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
